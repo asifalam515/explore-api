ff6a652495969313a9fef00f6e9aa393b8f63abf
asifalam515 <asibulalam515@gmail.com>
2022-10-11 06:50:13 +0600

    flag added

---
+++ js/countries.js
@@ -0,0 +1,42 @@
+const loadCountries=()=>{
+    fetch('https://restcountries.com/v3.1/all')
+    .then(res=>res.json())
+    .then(data=>displayCountris(data))
+}
+const displayCountris=(countries)=>{
+    // for(const country of countries){
+    //     console.log(country);
+    // }
+
+    const countriesContainer=document.getElementById('countries-container')
+countries.forEach(country=>{
+    // console.log(country)
+   const countryDiv=document.createElement('div')
+   countryDiv.classList.add('country')
+   countryDiv.innerHTML=`
+   <h3>Name:${country.name.common}</h3>
+   <p>Capital:${country.capital?country.capital[0]:'No capital'}</p>
+   
+   <button onclick="loadCountryDetail('${country.cca2}')">Details</button>
+   `
+   countriesContainer.appendChild(countryDiv)
+})
+}
+
+const loadCountryDetail=(code)=>{
+//  https://restcountries.com/v2/alpha/{code}
+const url=`https://restcountries.com/v2/alpha/${code}`
+fetch(url)
+.then(res=>res.json())
+.then(data=>displayCountryDetails(data))
+}
+
+const displayCountryDetails=(country)=>{
+    console.log(country)
+   const countryDetail=document.getElementById('country-detail')
+   countryDetail.innerHTML=`
+  <img src="${country.flags.png}"> 
+   `
+}
+
+loadCountries()
